ff1a622e153e42b02b0c43947d2d3380a7bfb692
Marvel0007 <shivam.2024ug1088@iiitranchi.ac.in>
2025-07-15 11:40:50 +0000

    yes

--- 01_basics/07_datetime.js
+++ 01_basics/07_datetime.js
@@ -16,3 +16,13 @@ let mmyTimeStamp = Date.now()
 // console.log(myCreateDate.getTime());
 console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
