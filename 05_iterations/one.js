9e055d9a736a7346958580752d9616b544101854
Shivam Shukla <shivam.2024ug1088@iiitranchi.ac.in>
2025-08-14 06:14:29 +0000

    Yes

---
+++ 05_iterations/one.js
@@ -0,0 +1,19 @@
+// for
+
+for (let i = 1; i <= 5; i++) {
+    const element = i;
+    console.log(element);   
+}
+
+// while
+
+let index = 0
+while (index<=5) {
+    console.log(`Value: ${index}`);
+    index++;
+}
+
+do {
+    console.log(`Value: ${index}`);
+    index++;
+} while (index<=10);
